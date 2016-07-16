da7792dd7103ba646305f078a634689cdff323cf
Rafa Moreno <rmoreno.cesar@gmail.com>
2016-07-16 15:54:05 -0700

    fixed up setting screen

--- app/src/main/java/net/rmoreno/weatherapp/SettingsActivity.java
+++ app/src/main/java/net/rmoreno/weatherapp/SettingsActivity.java
@@ -6,13 +6,18 @@ import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.ParseException;
 import java.util.Map;
 
 
@@ -33,26 +38,46 @@ public class SettingsActivity extends ActionBarActivity {
         mDone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (isValidInput((mTemperature.getText().toString()))) {
+                    SharedPreferences preferences = getSharedPreferences(SHARED_PREFERENCES, Context.MODE_PRIVATE);
+                    SharedPreferences.Editor edit = preferences.edit();
 
-                SharedPreferences preferences = getSharedPreferences(SHARED_PREFERENCES, Context.MODE_PRIVATE);
-                SharedPreferences.Editor edit = preferences.edit();
-                
-                int updateSweater = Integer.parseInt(mTemperature.getText().toString());
-                edit.remove("sweater");
-                edit.putInt("sweater", updateSweater);
-                edit.commit();
-                preferences.getInt("sweater", 0);
+                    int updateSweater = Integer.parseInt(mTemperature.getText().toString());
+                    edit.remove("sweater");
+                    edit.putInt("sweater", updateSweater);
+                    edit.commit();
+                    preferences.getInt("sweater", 0);
 
-                Toast.makeText(SettingsActivity.this, "Sweater Weather Updated", Toast.LENGTH_SHORT).show();
-
-                Map<String, ?> map = preferences.getAll();
-                for(Map.Entry<String,?> entry : map.entrySet()){
-                    Log.d("map values",entry.getKey() + ": " +
-                            entry.getValue().toString());
+                    displayToast("Sweater Weather Updated!").show();
+                    Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+                    startActivity(intent);
                 }
-                Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
-                startActivity(intent);
+
+                else {
+                    mTemperature.setText("");
+                    displayToast("Invalid input").show();
+                }
             }
         });
     }
+
+    private boolean isValidInput(String input) {
+        try {
+            Integer.parseInt(input);
+            if(input.length() > 0) {
+                return true;
+            }
+
+        } catch(NumberFormatException e) {
+            Log.e("Invalid input", e.getMessage());
+            return false;
+        }
+
+        return true;
+    }
+
+    private Toast displayToast(String display) {
+        return Toast.makeText(SettingsActivity.this, display, Toast.LENGTH_SHORT);
+    }
+
 }
