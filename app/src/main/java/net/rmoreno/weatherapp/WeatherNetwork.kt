b56c4fd3a275614bc3f3dbc7b63bc29db96eb443
rafa <rmoreno.cesar@gmail.com>
2017-09-24 23:04:57 -0500

    updated rxandroid in gradle

---
+++ app/src/main/java/net/rmoreno/weatherapp/WeatherNetwork.kt
@@ -0,0 +1,36 @@
+package net.rmoreno.weatherapp
+
+import com.squareup.okhttp.Callback
+import com.squareup.okhttp.OkHttpClient
+import com.squareup.okhttp.Request
+import com.squareup.okhttp.Response
+
+import java.io.IOException
+
+import io.reactivex.Observable
+import io.reactivex.ObservableEmitter
+import io.reactivex.ObservableOnSubscribe
+import io.reactivex.annotations.NonNull
+
+class WeatherNetwork {
+
+    fun getWeather(lat: Double, lng: Double, callback: Callback): Observable<Response> {
+        val URL = "https://api.forecast.io/forecast/5530508d3568e57848d53bf10cfade1f/$lat,$lng"
+        val client = OkHttpClient()
+        val request = Request.Builder()
+                .url(URL)
+                .build()
+
+
+        return Observable.create { em ->
+            try {
+                val response = client.newCall(request).execute()
+                em.onNext(response)
+                em.onComplete()
+            } catch (err: IOException) {
+                err.printStackTrace()
+                em.onError(err)
+            }
+        }
+    }
+}
