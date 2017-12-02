c24a2a1cc5346a04a62e2b4e536b4cfe8b4cb495
rafa <rmoreno.cesar@gmail.com>
2017-12-02 15:40:12 -0600

    extracted all location logic from MainActivity

---
+++ app/src/main/java/net/rmoreno/weatherapp/LocationSensor.kt
@@ -0,0 +1,7 @@
+package net.rmoreno.weatherapp
+
+/**
+ * Created by Rafa on 12/2/17.
+ */
+class LocationSensor {
+}
--- app/src/main/java/net/rmoreno/weatherapp/ui/MainActivity.kt
+++ app/src/main/java/net/rmoreno/weatherapp/ui/MainActivity.kt
@@ -4,18 +4,16 @@ import android.app.Activity
 import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
-import android.location.LocationManager
 import android.net.ConnectivityManager
 import android.os.Bundle
 import android.provider.Settings
 import android.support.v7.widget.LinearLayoutManager
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-import net.rmoreno.weatherapp.MyLocationListener
+import net.rmoreno.weatherapp.LocationSensor
 import net.rmoreno.weatherapp.R
 import net.rmoreno.weatherapp.WeatherInteractor
 import net.rmoreno.weatherapp.adapters.DailyAdapter
@@ -29,46 +27,34 @@ import java.util.*
 
 class MainActivity : Activity(), WeatherView {
 
-    internal var ACTIVITY = "MAIN ACTIVITY"
-    internal var SHARED_PREFERENCES = "MyPrefs"
+    //internal var ACTIVITY = "MAIN ACTIVITY"
+    private var SHARED_PREFERENCES = "MyPrefs"
 
     lateinit var weatherPresenter: WeatherPresenter
 
-    internal var REQUEST_CODE = 100
+    private var REQUEST_CODE = 100
+
+    lateinit var connectivityManager: ConnectivityManager
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //set sharedPreferences preferences to detect if user has a 'sweater weather'
-        val sharedPreferences = getSharedPreferences(SHARED_PREFERENCES, Context.MODE_PRIVATE)
-
-        weatherPresenter = WeatherPresenterImpl(
-                this,
-                WeatherInteractor(
-                        WeatherRepository(sharedPreferences)
-                )
-        )
-
-        if (weatherPresenter.isUsersFirstTime) {
-            val intent = Intent(this@MainActivity, IntroActivity::class.java)
-            startActivity(intent)
-            Toast.makeText(this@MainActivity, "sweater is 0", Toast.LENGTH_SHORT).show()
-        }
 
         recycler_view.layoutManager = LinearLayoutManager(this@MainActivity)
         weather_loading_bar.setVisibility(View.VISIBLE)
         weather_loading_bar.bringToFront()
 
+        weatherPresenter.checkIfFirstTime()
 
-        if (isNetworkAvailible()) {
-            getLocation()
-        }
+        setup()
     }
 
     public override fun onResume() {
         super.onResume()
-        weatherPresenter.resume()
-        getLocation()
+        //TODO unbind presenter and connectivity manager
+        setup()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
@@ -76,31 +62,23 @@ class MainActivity : Activity(), WeatherView {
 
     }
 
-    private fun isNetworkAvailible(): Boolean {
-        val manager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        val networkInfo = manager.activeNetworkInfo
-        var isAvailable = false
-        if (networkInfo != null && networkInfo.isConnected) {
-            isAvailable = true
-        }
-        return isAvailable
+    private fun setup() {
+        val sharedPreferences = getSharedPreferences(SHARED_PREFERENCES, Context.MODE_PRIVATE)
+        val locationSensor = LocationSensor(this@MainActivity)
+        val weatherRepository = WeatherRepository(sharedPreferences)
+        val weatherInteractor = WeatherInteractor(weatherRepository, locationSensor)
+
+        weatherPresenter = WeatherPresenterImpl(
+                this,
+                    weatherInteractor
+        )
+
     }
 
-    fun isLocationEnabled(lm: LocationManager): Boolean {
-        var gps_enabled = false
-        var network_enabled = false
-
-        try {
-            gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER)
-        } catch (ex: Exception) {
-        }
-
-        try {
-            network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
-        } catch (ex: Exception) {
-        }
-
-        return gps_enabled && network_enabled
+    override fun goToIntroActivity() {
+        val intent = Intent(this@MainActivity, IntroActivity::class.java)
+        startActivity(intent)
+        Toast.makeText(this@MainActivity, "sweater is 0", Toast.LENGTH_SHORT).show()
     }
 
     override fun displayDailyWeather(dailyWeather: ArrayList<DailyWeather>, sweaterTemp: Int) {
@@ -118,7 +96,18 @@ class MainActivity : Activity(), WeatherView {
         wind.text = currentWeather.wind.toString() + "mph"
     }
 
-    fun buildDialog(context: Context) {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun setLoading(isLoading: Boolean) {
+        weather_loading_bar.setVisibility(if (isLoading) View.VISIBLE else View.GONE);
+    }
+
+    override fun displayNetworkError() {
+        val context: Context = this@MainActivity
+
         val dialog = AlertDialog.Builder(context)
         dialog.setMessage("GPS network is not enabled")
         dialog.setPositiveButton(context.resources.getString(R.string.open_location_settings)) { _, _ ->
@@ -130,43 +119,23 @@ class MainActivity : Activity(), WeatherView {
         dialog.show()
     }
 
-    fun getLocation() {
-        val lm = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        val locationListener = MyLocationListener()
-
-        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0f, locationListener)
-        if (isLocationEnabled(lm)) {
-            val location = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
-            Log.d("LOCATION", location.latitude.toString())
-            weatherPresenter.getCurrentWeather(location.latitude, location.longitude)
-            weatherPresenter.getDailyWeather(location.latitude, location.longitude)
-
-        } else {
-            buildDialog(this@MainActivity)
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun setLoading(isLoading: Boolean) {
-        weather_loading_bar.setVisibility(if (isLoading) View.VISIBLE else  View.GONE);
-    }
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val id = item.itemId
 
-        if (id == R.id.action_settings) {
-            return true
-        } else if (id == R.id.set_sweather) {
-            val intent = Intent(this@MainActivity, SettingsActivity::class.java)
-            startActivity(intent)
-        } else if (id == R.id.refresh) {
-            getLocation()
-        }
+        when (id) {
+            R.id.action_settings -> {
+                return true
+            }
 
+            R.id.set_sweather -> {
+                val intent = Intent(this@MainActivity, SettingsActivity::class.java)
+                startActivity(intent)
+            }
+
+            R.id.refresh -> {
+                setup()
+            }
+        }
         return super.onOptionsItemSelected(item)
     }
 }
