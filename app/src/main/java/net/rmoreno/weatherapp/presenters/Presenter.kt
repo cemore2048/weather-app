a6065d5de8f5dfbe6b979e5a356bae081aa059e6
rafa <rmoreno.cesar@gmail.com>
2017-12-03 19:36:28 -0600

    updated Presenter class so that it's actually doing something

--- app/src/main/java/net/rmoreno/weatherapp/presenters/Presenter.kt
+++ app/src/main/java/net/rmoreno/weatherapp/presenters/Presenter.kt
@@ -1,9 +1,35 @@
 package net.rmoreno.weatherapp.presenters
 
-// this base presenter is crap. Figure out what to do with it
-interface Presenter {
+import java.lang.ref.WeakReference
 
-    fun resume()
+abstract class Presenter<M, V> {
 
-    fun destroy()
+    private var model: M? = null
+    private var view: WeakReference<V>? = null
+
+    fun setModel(model: M) {
+        this.model = model
+
+        if (setupDone()) {
+            updateView()
+        }
+    }
+
+    fun bindView(view: V) {
+        this.view = WeakReference(view)
+    }
+
+    abstract fun updateView()
+
+    fun view(): V? {
+        return if (view == null) null else view?.get()
+    }
+
+    private fun setupDone(): Boolean {
+        return this.view != null && this.model != null
+    }
+
+    open fun unbindView() {
+        this.view = null
+    }
 }
